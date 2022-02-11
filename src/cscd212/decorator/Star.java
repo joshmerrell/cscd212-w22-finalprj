cddbba879d115c657a32d84f80411a8533fa6c2f
Evelyn Barragan <Ebarragan5@ewu.edu>
2022-02-10 21:48:16 -0800

    added Star

--- src/cscd212/decorator/Star.java
+++ src/cscd212/decorator/Star.java
@@ -1,19 +1,20 @@
 package cscd212.decorator;
+
 import cscd212.classes.ChristmasTree;
 
-public class BoredEmoji extends TreeDecorator{
+public class Star extends TreeDecorator{
     ChristmasTree christmasTree;
 
-    public BoredEmoji(ChristmasTree christmasTree){
+    public Star(ChristmasTree christmasTree){
         this.christmasTree = christmasTree;
     }
     @Override
     public double cost() {
-        return 25.00 + christmasTree.cost();
+        return 50.00 + christmasTree.cost();
     }
 
     @Override
     public String getDescription() {
-        return christmasTree.getDescription() + ", Bored Emoji Ornament";
+        return christmasTree.getDescription() + ", Christmas Star";
     }
 }
