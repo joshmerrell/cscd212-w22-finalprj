1dada7ac1560887ddab1946dda2b76c7356cdf7b
Evelyn Barragan <Ebarragan5@ewu.edu>
2022-02-10 19:48:54 -0800

    Changed name to Palm Tree
    added DVC, and cost

--- cscd212/classes/ChristmasTreeImplementation.java
+++
@@ -1,9 +0,0 @@
-package cscd212.classes;
-import cscd212.abstractclasses.ChristmasTree;
-
-public class ChristmasTreeImplementation extends ChristmasTree {
-    @Override
-    public String decorate() {
-        return "Christmas Tree";
-    }
-}
---
+++ cscd212/classes/PalmTree.java
@@ -0,0 +1,13 @@
+package cscd212.classes;
+import cscd212.abstractclasses.ChristmasTree;
+
+public class PalmTree extends ChristmasTree {
+
+    public PalmTree(){
+        description = "Palm Tree";
+    }
+
+    public double cost(){
+        return 999.99;
+    }
+}
