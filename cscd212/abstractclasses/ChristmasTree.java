19279c143201efa0c9a92cb21a3bde41f783b295
Evelyn Barragan <Ebarragan5@ewu.edu>
2022-02-10 19:37:59 -0800

    updated Christmas Tree abstract
    added description and cost

--- cscd212/abstractclasses/ChristmasTree.java
+++ cscd212/abstractclasses/ChristmasTree.java
@@ -1,7 +1,12 @@
 package cscd212.abstractclasses;
 
 public abstract class ChristmasTree {
-    public String decorate() {
-        return null;
+    String description = "Unknown type of tree";
+
+    public String getDescription() {
+        return description;
     }
-}
+    public abstract double cost();
+    //public String decorate() {
+        //return null;
+    }
